a83fba5431f7ee361b2d2e9aed9fc3b4b943541e
Cruzer-S <yyyynoom@gmail.com>
2023-05-20 16:31:38 +0900

    match class name to filename

--- Assets/Scripts/Permission Scene Example.cs
+++ Assets/Scripts/Permission Scene Example.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 using TMPro;
 
-public class GameManager : MonoBehaviour
+public class PermissionSceneExample : MonoBehaviour
 {
     [SerializeField] private TMP_Text result;
 
